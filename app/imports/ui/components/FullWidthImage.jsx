e53a2da53329eae30d1eb98b49e300a7f18cce61
yuumayamamoto <46611050+yuumayamamoto@users.noreply.github.com>
2019-10-22 11:54:20 -1000

    port

--- app/imports/ui/components/FullWidthImage.jsx
+++ app/imports/ui/components/FullWidthImage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Menu, Grid, Dropdown } from 'semantic-ui-react';
+import { Image } from 'semantic-ui-react';
 
 
-class  FullWidthImage extends React.COmponents {
-  render()
-  return (
-      <Image fluid src = "cdn"
-  )
+export default class FullWidthImage extends React.COmponents {
+  render() {
+    return (
+        <Image fluid src="//cdn.shopify.com/s/files/1/1035/5187/t/5/assets/slidel.jpg?9243197903709543590"/>
+    );
+  }
 }
--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -4,9 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import 'semantic-ui-css/semantic.css';
 import { Roles } from 'meteor/alanning:roles';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import Landing from '../pages/Landing';
 import ListStuff from '../pages/ListStuff';
 import ListStuffAdmin from '../pages/ListStuffAdmin';
 import AddStuff from '../pages/AddStuff';
@@ -15,6 +13,9 @@ import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
+import IslandSnow from '../pages/islandSnow';
+import TopMenu from '../components/TopMenu';
+import FooterMenu from '../components/FooterMenu';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -22,9 +23,9 @@ class App extends React.Component {
     return (
         <Router>
           <div>
-            <NavBar/>
+            <TopMenu/>
             <Switch>
-              <Route exact path="/" component={Landing}/>
+              <Route exact path="/" component={IslandSnow}/>
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
               <ProtectedRoute path="/list" component={ListStuff}/>
@@ -34,7 +35,7 @@ class App extends React.Component {
               <ProtectedRoute path="/signout" component={Signout}/>
               <Route component={NotFound}/>
             </Switch>
-            <Footer/>
+            <FooterMenu/>
           </div>
         </Router>
     );
