c3b178700a718388b932de886f7b699a0b35f1f2
yuumayamamoto <46611050+yuumayamamoto@users.noreply.github.com>
2019-10-22 12:18:14 -1000

    done

--- app/imports/ui/components/FullWidthImage.jsx
+++ app/imports/ui/components/FullWidthImage.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Image } from 'semantic-ui-react';
 
 
-export default class FullWidthImage extends React.COmponents {
+export default class FullWidthImage extends React.Component {
   render() {
     return (
-        <Image fluid src="//cdn.shopify.com/s/files/1/1035/5187/t/5/assets/slidel.jpg?9243197903709543590"/>
+        <Image fluid src="//cdn.shopify.com/s/files/1/1035/5187/t/5/assets/slide1.jpg?9243197903709543590"/>
     );
   }
 }
